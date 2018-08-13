468acacf3625d67b5ab61aa23915ba96498bbe3e
Noah Davis <noahdavis319@gmail.com>
2018-08-13 18:03:08 -0400

    Added __init__.py to build.py filter_resources_glob

--- build.py
+++ build.py
@@ -11,6 +11,7 @@ use_plugin('python.coverage')
 use_plugin('python.distutils')
 use_plugin('python.pycharm')
 
+use_plugin("filter_resources")
 use_plugin('copy_resources')
 
 name = 'gitpage'
@@ -19,6 +20,9 @@ default_task = 'publish'
 
 @init
 def set_properties(project):
+    project.build_depends_on('service')
+    project.depends_on('flask')
+
     project.set_property('coverage_threshold_warn', 85)
     project.set_property('coverage_branch_threshold_warn', 85)
     project.set_property('coverage_branch_partial_threshold_warn', 85)
@@ -27,8 +31,7 @@ def set_properties(project):
 
     project.set_property('distutils_console_scripts', ['gitpage = cli:cli'])
 
-    project.build_depends_on('service')
-    project.depends_on('flask')
+    project.get_property("filter_resources_glob").append("**/gitpage/__init__.py")
 
     project.get_property('copy_resources_glob').append(
         'src/main/python/templates/*')
